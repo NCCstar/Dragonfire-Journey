01e4e6d000c07e954a646eb298105ad6be6e2e49
Rhys Miller <millerpscott@LCARS.home>
2016-05-19 21:15:35 -0400

    Comments and Methodizing

--- QuestBoard.java
+++ QuestBoard.java
@@ -11,26 +11,29 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 public class QuestBoard extends JPanel implements MouseListener
 {
-   private static SparseMatrix<Tile> grid;
-   private static final int DIM=60;
-   private Hero[] players;
-   private int p=0;
+   private static SparseMatrix<Tile> grid;//game board
+   private static final int DIM=60;//unversal size of one grid tile
+   private Hero[] players;//array of the player Heroes, .length for number of players
+   private int p=0;//player number active
    private int mode=0;//0=board-movement, 1=cards, 2=battle
-   private int enemy=0;
-   private String enemyName=null;
+   private int enemy=0;//amount of enemy hp remaining
+   private String enemyName=null;//String of the enemy's name - used to randomize enemy hp
+   //pre: pNum-number of players, decided by QuestDriver
+   //post: creates QuestBoard w/ appropriate values.
    public QuestBoard(int pNum)
    {
       addMouseListener(this);
    
       grid=new SparseMatrix(10,13);
-      
+      //add starting tiles: dragon lair
       grid.add(4,6,new Tile(true,new boolean[]{true,true,true,true}));
       grid.add(5,6,new Tile(true,new boolean[]{true,true,true,true}));
+      //add starting corners
       grid.add(0,0,new Tile(true,new boolean[]{false,true,true,false}));
       grid.add(0,12,new Tile(true,new boolean[]{false,false,true,true}));
       grid.add(9,0,new Tile(true,new boolean[]{true,true,false,false}));
       grid.add(9,12,new Tile(true,new boolean[]{true,false,false,true}));
-      
+      //define players
       players=new Hero[pNum];
       for(int i=0;i<players.length;i++)
       {
@@ -52,117 +55,86 @@ public class QuestBoard extends JPanel implements MouseListener
          }
       }
    }
+   //pre: dir is direction of movement, 0=up,1=right,2=down,3=left
+   //post: moves player, adds tile if needed
+   private void newTile(int dir)
+   {
+      int opp=(dir+2)%4;//opposite direction
+      if(grid.get(players[p].getY(),players[p].getX()).getExits()[dir])//if this room has dir exit
+      {
+         switch(dir)
+         {
+            case 0:
+               players[p].moveY(false);
+               break;
+            case 1:
+               players[p].moveX(true);//move the palyer
+               break;
+            case 2:
+               players[p].moveY(true);
+               break;
+            default:
+               players[p].moveX(false);
+               break;
+         }
+         if(grid.get(players[p].getY(),players[p].getX())==null)//if new space is empty
+         {
+            //add new random tile - has small chance of infinite while loop.
+            grid.add(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
+            while(!grid.get(players[p].getY(),players[p].getX()).getExits()[opp])//while no exit on this side
+            {
+               grid.get(players[p].getY(),players[p].getX()).rotate();//rotate the square
+            }
+         }
+      }
    
+   }
    public void mouseClicked(MouseEvent e)
    {
-      if(mode==0)
+      if(mode==0)//if drawing main board
       {
-         int x=e.getX()/DIM;
+         int x=e.getX()/DIM;//get translate coordinates of click
          int y=e.getY()/DIM;
-         if(false)
-            if(e.getButton()==MouseEvent.BUTTON1)
-            {
-               grid.add(y,x,new Tile(false,new boolean[]{u.ranB(.6),u.ranB(.6),u.ranB(.6),u.ranB(.6)}));
-            }
-            else
-            {
-               grid.get(y,x).rotate();
-            }
-         
-         if(x==players[p].getX()-1&&y==players[p].getY())//left
+         //curr pos = current position
+         if(x==players[p].getX()&&y==players[p].getY()-1)//if clicked above player
          {
-            if(grid.get(players[p].getY(),players[p].getX()).getExits()[3])
-            {
-               players[p].moveX(false);
-               if(grid.get(players[p].getY(),players[p].getX())==null)
-               {
-                  grid.add(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
-                  while(!grid.get(players[p].getY(),players[p].getX()).getExits()[1])
-                  {
-                     grid.set(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
-                  }
-               }
-               if(grid.get(players[p].getY(),players[p].getX()).isRoom())
-               {
-                  exeCard(drawRoomCard());
-                  p++;p%=players.length;
-               }
-            }
+            newTile(0);
          }
-         if(x==players[p].getX()&&y==players[p].getY()-1)//up
+         if(x==players[p].getX()+1&&y==players[p].getY())//if clicked right of player
          {
-            if(grid.get(players[p].getY(),players[p].getX()).getExits()[0])
-            {
-               players[p].moveY(false);
-               if(grid.get(players[p].getY(),players[p].getX())==null)
-               {
-                  grid.add(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
-                  while(!grid.get(players[p].getY(),players[p].getX()).getExits()[2])
-                  {
-                     grid.set(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
-                  }
-               }
-               if(grid.get(players[p].getY(),players[p].getX()).isRoom())
-               {
-                  exeCard(drawRoomCard());
-                  p++;p%=players.length;
-               }
-            }
+            newTile(1);
          }
-         if(x==players[p].getX()+1&&y==players[p].getY())//right
+         if(x==players[p].getX()&&y==players[p].getY()+1)//if clicked below player
          {
-            if(grid.get(players[p].getY(),players[p].getX()).getExits()[1])
-            {
-               players[p].moveX(true);
-               if(grid.get(players[p].getY(),players[p].getX())==null)
-               {
-                  grid.add(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
-                  while(!grid.get(players[p].getY(),players[p].getX()).getExits()[3])
-                  {
-                     grid.set(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
-                  }
-               }
-               if(grid.get(players[p].getY(),players[p].getX()).isRoom())
-               {
-                  exeCard(drawRoomCard());
-                  p++;p%=players.length;
-               }
-            }
+            newTile(2);
          }
-         if(x==players[p].getX()&&y==players[p].getY()+1)//down
+         if(x==players[p].getX()-1&&y==players[p].getY())//if clicked to left of curr pos
          {
-            if(grid.get(players[p].getY(),players[p].getX()).getExits()[2])
-            {
-               players[p].moveY(true);
-               if(grid.get(players[p].getY(),players[p].getX())==null)
-               {
-                  grid.add(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
-                  while(!grid.get(players[p].getY(),players[p].getX()).getExits()[0])
-                  {
-                     grid.set(players[p].getY(),players[p].getX(),new Tile(u.ranB(.9),new boolean[]{u.ranB(),u.ranB(),u.ranB(),u.ranB()}));
-                  }
-               }
-               if(grid.get(players[p].getY(),players[p].getX()).isRoom())
-               {
-                  exeCard(drawRoomCard());
-                  p++;p%=players.length;
-               }
-            }
+            newTile(3);
          }
+         if(grid.get(players[p].getY(),players[p].getX()).isRoom())//if this is not a corridor
+         {
+            exeCard(drawRoomCard());//draw and execute a room card
+            p++;p%=players.length;//next player.
+         }//else keep same player
       }
       else
-      if(mode==2)
-      {
+         if(mode==2)//if in battle mode
+         {
          
-      }
+         }
       repaint();
    }
+   //pre: card - identity of card to be processed
+   //post: 
    private void exeCard(String card)
    {
-      u.SOP(card);
+      u.SOP(card);//DEBUG
       switch(card)
       {
-         case "goblin":
+         case "goblin suprise":
+         //attack damage
+         case "goblin"://if goblin monster
             mode=2;
             enemyName="Goblin";
             enemy = u.ranI(2,4);
@@ -174,7 +146,7 @@ public class QuestBoard extends JPanel implements MouseListener
             break;
       }
    }
-   
+   //post: returns random room card
    private String drawRoomCard()
    {
       int ran=u.ranI(0,50);
@@ -218,20 +190,30 @@ public class QuestBoard extends JPanel implements MouseListener
             g.setColor(Color.red.darker());
             g.fillRect(DIM*3,0,DIM*3,DIM*2);
             g.fillRect(DIM*3,DIM*7,DIM*3,DIM*2);
+            
             g.setColor(Color.blue.darker());
             g.fillRect(0,DIM*3,DIM*2,DIM*3);
             g.fillRect(DIM*7,DIM*3,DIM*2,DIM*3);
+            
             g.setColor(Color.red.brighter());
             g.fillRect(DIM*3,DIM*2,DIM*3,DIM);
             g.fillRect(DIM*3,DIM*6,DIM*3,DIM);
+            
             g.setColor(Color.blue.brighter());
             g.fillRect(DIM*2,DIM*3,DIM,DIM*3);
             g.fillRect(DIM*6,DIM*3,DIM,DIM*3);
             
             g.setColor(Color.black);
-            g.drawRect(DIM*3,DIM*3,DIM*3,DIM*3);
             g.drawRect(DIM*3,DIM*2,DIM*3,DIM);
             g.drawRect(DIM*3,DIM*6,DIM*3,DIM);
+            g.drawRect(DIM*4,DIM*2,DIM,DIM);
+            g.drawRect(DIM*4,DIM*6,DIM,DIM);
+            g.drawRect(DIM*2,DIM*3,DIM,DIM*3);
+            g.drawRect(DIM*2,DIM*4,DIM,DIM);
+            g.drawRect(DIM*6,DIM*3,DIM,DIM*3);
+            g.drawRect(DIM*6,DIM*4,DIM,DIM);
+            
+            
          }
       
       g.drawString(p+1+"",0,DIM*10+DIM/2);
