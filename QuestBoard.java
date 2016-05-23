a90131e5cd973332f4aed88a0945c8e90fb049ae
NCCstar <rhyspmiller@gmail.com>
2016-05-23 14:54:35 -0400

    Message to future: work on when card drawn

--- QuestBoard.java
+++ QuestBoard.java
@@ -15,9 +15,9 @@ public class QuestBoard extends JPanel implements MouseListener
    private static final byte DIM=60;//unversal size of one grid tile
    private Hero[] players;//array of the player Heroes, .length for number of players
    private int p=0;//player number active
-   private boolean secTun=false;
+   private boolean secTun=false;//if in secret tunnel mode
    private byte dragonLeft = 8;//dragon cards left
-   public int getDIM(){
+   public byte getDIM(){
       return DIM;}
    //pre: pNum-number of players, decided by QuestDriver
    //post: creates QuestBoard w/ appropriate values.
@@ -61,7 +61,7 @@ public class QuestBoard extends JPanel implements MouseListener
    private void newTile(int dir)
    {
       int opp=(dir+2)%4;//opposite direction
-      if(grid.get(players[p].getY(),players[p].getX()).getExits()[dir])//if this room has dir exit
+      if(grid.get(players[p].getY(),players[p].getX()).getExits()[dir]||secTun)//if this room has dir exit or in secTun mode
       {
          switch(dir)
          {
@@ -98,6 +98,7 @@ public class QuestBoard extends JPanel implements MouseListener
          }
       }
    }
+   //post: returns true if one and only one in array is true
    private boolean oneTrue(boolean[] array)
    {
       int count=0;
@@ -115,44 +116,73 @@ public class QuestBoard extends JPanel implements MouseListener
          //curr pos = current position
       if(x>=0&&x<13&&y>=0&&y<10)
       {
-         boolean legit=false;//should change turn
-         if(x==players[p].getX()&&y==players[p].getY()&&grid.get(y,x).canSearch())
+         boolean legit=false;//if should change turn
+         do//loop to break out of
          {
-            if(x==6&&(y==4||y==5))
-               exeCard("dragon");
-            else
+            if(x==players[p].getX()&&y==players[p].getY()&&grid.get(y,x).canSearch())//if searching
             {
-               exeCard(drawCard((byte)1));
-               grid.get(y,x).search();
+               if(x==6&&(y==4||y==5))
+                  exeCard("dragon");//if in dragon room, get dragon loot
+               else
+               {
+                  exeCard(drawCard((byte)1));//random search card
+                  grid.get(y,x).search();//increment tile's search
+               }
+               if(!secTun)
+                  legit=true;
+               break;//don't check the rest
             }
-            p=(p+1)%players.length;
-         }
-         if(x==players[p].getX()&&y==players[p].getY()-1)//if clicked above player
-         {
-            newTile(0);
-            legit=true;
-         }
-         if(x==players[p].getX()+1&&y==players[p].getY())//if clicked right of player
-         {
-            newTile(1);
-            legit=true;
-         }
-         if(x==players[p].getX()&&y==players[p].getY()+1)//if clicked below player
-         {
-            newTile(2);
-            legit=true;
-         }
-         if(x==players[p].getX()-1&&y==players[p].getY())//if clicked to left of curr pos
-         {
-            newTile(3);
-            legit=true;
-         }
-         if(grid.get(players[p].getY(),players[p].getX()).isRoom()&&legit)//if this is not a corridor
-         {
-            if(x==6&&(y==4||y==5))
-               exeCard("dragon");
+            if(x==players[p].getX()&&y==players[p].getY()-1)//if clicked above player
+            {
+               newTile(0);//go up
+               if(secTun)
+               {//un-secret tunnel
+                  secTun=false;
+               }
+               legit=true;//next turn
+            }
+            if(x==players[p].getX()+1&&y==players[p].getY())//if clicked right of player
+            {
+               newTile(1);//go right
+               if(secTun)
+               {//un-secret tunnel
+                  secTun=false;
+               }
+               legit=true;//next turn
+            }
+            if(x==players[p].getX()&&y==players[p].getY()+1)//if clicked below player
+            {
+               newTile(2);//go down
+               if(secTun)
+               {//un-secret tunnel
+                  secTun=false;
+               }
+               legit=true;//next turn
+            }
+            if(x==players[p].getX()-1&&y==players[p].getY())//if clicked to left of curr pos
+            {
+               newTile(3);//go left
+               if(secTun)
+               {//un-secret tunnel
+                  secTun=false;
+               }
+               legit=true;//next turn
+            }
+            if(grid.get(players[p].getY(),players[p].getX()).isRoom())//if this is not a corridor
+            {
+               if(x==6&&(y==4||y==5))
+                  exeCard("dragon");//if in chamber, dragon card
+               else
+                  exeCard(drawCard((byte)0));//draw and execute a room card
+            }
             else
-               exeCard(drawCard((byte)0));//draw and execute a room card
+               if(!grid.get(players[p].getY(),players[p].getX()).isRoom())//if corridor
+               {
+                  legit=false;
+               }
+         }while(false);
+         if(legit)
+         {
             p=(p+1)%players.length;//next player.
          }//else keep same player
       }
@@ -177,18 +207,23 @@ public class QuestBoard extends JPanel implements MouseListener
             repaint();
             battle("Orc",(byte)u.ranI(3,5));
             break;
+         case "champion":
+            //transition
+            repaint();
+            battle("Champion of Chaos",(byte)u.ranI(6,8));
+            break;
          case "dragon":
-            if(u.ranI(0,dragonLeft)==0)
-            {
+            if(u.ranI(0,dragonLeft)==0)//if random between 0 and num of dragon cards left is 0
+            {//wake up dragon
                JOptionPane.showMessageDialog(null,"The dragon awakes!","ROAAAR!!",JOptionPane.INFORMATION_MESSAGE);
-               players[p].changeHP(u.ranI(-10,-20));
+               players[p].changeHP(u.ranI(-10,-20));//deal 10-20 damage
             }
             else
-            {
-               dragonLeft--;
-               int gold = u.ranI(10,100)*100;
+            {//else
+               dragonLeft--;//decrese num of dragon cards left
+               int gold = u.ranI(10,100)*100;//random gold
                JOptionPane.showMessageDialog(null,"You snatch "+gold+"G of the dragon's treasure.","ZZzzzz",JOptionPane.INFORMATION_MESSAGE);
-               players[p].getBag().add(gold+"G");
+               players[p].getBag().add(gold+"G");//add gold to bag
             }
             break;
          case "gold":
@@ -198,11 +233,10 @@ public class QuestBoard extends JPanel implements MouseListener
             break;
          case "secret tunnel":
             JOptionPane.showMessageDialog(null,"You find a secret passage. Move to an adjecent space.","Secret Tunnel!",JOptionPane.INFORMATION_MESSAGE);
-            
-            break;
-         case "nothing":
-            JOptionPane.showMessageDialog(null,"Nothing happens.","Phew",JOptionPane.INFORMATION_MESSAGE); 
             secTun=true;
+            break;
+         case "nothing"://blank room/search card
+            JOptionPane.showMessageDialog(null,"Nothing happens.","Phew",JOptionPane.INFORMATION_MESSAGE); 
             break;
       }
    }
@@ -224,9 +258,9 @@ public class QuestBoard extends JPanel implements MouseListener
                   break;
             }
          }
-         catch(Exception e)
+         catch(Exception e)//if click cancel
          {
-            exe=0;
+            exe=0;//leap aside
          }
          byte otherExe=0;
          float ran=(float)Math.random();
@@ -235,36 +269,51 @@ public class QuestBoard extends JPanel implements MouseListener
             case "Goblin":
                if(ran<.3)
                {
-                  otherExe=2;
+                  otherExe=2;//30% mighty blow
                }
                else
                   if(ran<.7)
                   {
-                     otherExe=1;
+                     otherExe=1;//40% slash
                   }
                   else
                   {
-                     otherExe=0;
+                     otherExe=0;//30%leap aside
+                  }
+               break;
+            case "Champion of Chaos":
+               if(ran<.3)
+               {
+                  otherExe=1;//30% slash
+               }
+               else
+                  if(ran<.8)
+                  {
+                     otherExe=2;//50% mighty blow
+                  }
+                  else
+                  {
+                     otherExe=0;//20% leap aside
                   }
                break;
             default:
-               if(ran<(1.0/3))
+               if(ran<(1.0/3))//33% all
                {
                   otherExe=2;
                }
                else
-                  if(ran<(2.0/3))
+                  if(ran<(2.0/3))//33% all
                   {
                      otherExe=1;
                   }
                   else
                   {
-                     otherExe=0;
+                     otherExe=0;//33% all
                   }
                break;
          }
          switch(otherExe)
-         {
+         {//print out what enemy does
             case 1:
                JOptionPane.showMessageDialog(null,"The "+enemyName+" slashes!",enemyName,JOptionPane.INFORMATION_MESSAGE);
                break;
@@ -275,26 +324,26 @@ public class QuestBoard extends JPanel implements MouseListener
                JOptionPane.showMessageDialog(null,"The "+enemyName+" leaps aside!",enemyName,JOptionPane.INFORMATION_MESSAGE);
                break;
          }
-         if(exe==otherExe)
+         if(exe==otherExe)//if same option
          {
-            enemyHP--;
+            enemyHP--;//deal 1 to each other
             players[p].changeHP(-1);
             JOptionPane.showMessageDialog(null,"1 damage to "+players[p].getName()+" and to the "+enemyName,"Damage!",JOptionPane.INFORMATION_MESSAGE);
          }
          else//0=la,1=sl,2=mb
-            if((exe+1)%3==otherExe)
+            if((exe+1)%3==otherExe)//if monster beat human
             {
-               players[p].changeHP(-1);
+               players[p].changeHP(-1);//human -1 HP
                JOptionPane.showMessageDialog(null,"1 damage to "+players[p].getName(),"Damage!",JOptionPane.INFORMATION_MESSAGE);
             }
-            else
+            else//Human has beaten monster at this point
                if(exe==2)
-               {
+               {//if mighty blow, deal 2 damage to monster
                   enemyHP-=2;
                   JOptionPane.showMessageDialog(null,"2 damage to the "+enemyName,"Uber-Damage!",JOptionPane.INFORMATION_MESSAGE);
                }
                else
-               {
+               {//if leap aside/slash deal 1 damage to monster
                   enemyHP--;
                   JOptionPane.showMessageDialog(null,"1 damage to the "+enemyName,"Damage!",JOptionPane.INFORMATION_MESSAGE);
                }
@@ -306,7 +355,7 @@ public class QuestBoard extends JPanel implements MouseListener
    {
       if(type==0)
       {
-         int ran=u.ranI(0,45);
+         int ran=u.ranI(0,50);
          if(ran<35)
          {
             return "nothing";
@@ -314,6 +363,10 @@ public class QuestBoard extends JPanel implements MouseListener
          if(ran<40)
          {
             return "goblin";
+         }
+         if(ran<45)
+         {
+            return "champion";
          }
          return "orc";
       }
@@ -330,7 +383,7 @@ public class QuestBoard extends JPanel implements MouseListener
          }
          return "gold";
       }
-      return "";
+      return "nothing";
    }
    public void paintComponent(Graphics g)
    {
@@ -347,29 +400,28 @@ public class QuestBoard extends JPanel implements MouseListener
                   switch(i)
                   {
                      case 0:
-                        g.setColor(Color.red);
+                        g.setColor(Color.red);//player 1
                         break;
                      case 1:
-                        g.setColor(Color.blue);
+                        g.setColor(Color.blue);//player 2
                         break;
                      case 2:
-                        g.setColor(Color.green);
+                        g.setColor(Color.green);//player 3
                         break;
                      case 3:
-                        g.setColor(Color.orange);
+                        g.setColor(Color.orange);//player 4
                         break;
                   }
-                  if(i!=p)
+                  if(i!=p)//if not curr player
                      g.setColor(g.getColor().darker());
                   g.fillRect(c*DIM+DIM/3,r*DIM+DIM/3,(int)(DIM-DIM/1.5),(int)(DIM-DIM/1.5));
-                  g.setColor(Color.black);
-                  g.drawString(""+players[i].getHP(),c*DIM+DIM/3+2,r*DIM+DIM/3+(int)(DIM-DIM/1.5)-2);
                }
             }
          }
       }
       drawPlayers(g);
    }
+   //post: draws the player cards at bottom of screen. Shows HP and items obtained.
    private void drawPlayers(Graphics g)
    {
       for(int i=0;i<players.length;i++)
@@ -377,33 +429,39 @@ public class QuestBoard extends JPanel implements MouseListener
          switch(i)
          {
             case 0:
-               g.setColor(Color.red);
+               g.setColor(Color.red);//player 1
                break;
             case 1:
-               g.setColor(Color.blue);
+               g.setColor(Color.blue);//player 2
                break;
             case 2:
-               g.setColor(Color.green);
+               g.setColor(Color.green);//player 3
                break;
             case 3:
-               g.setColor(Color.orange);
+               g.setColor(Color.orange);//player 4
                break;
          }
-         if(i==p)
+         if(i==p)//if current player
          {
-            Color temp=g.getColor();
-            g.setColor(Color.yellow);
+            Color temp=g.getColor();//save color
+            g.setColor(Color.yellow);//draw yellow indicator below portrait
             g.fillRect(i*DIM*2,DIM*12,DIM*2,DIM/2);
-            g.setColor(temp);
+            g.setColor(temp);//return color
          }
-         else
-            g.setColor(g.getColor().darker());
+         else//if not curr player
+            g.setColor(g.getColor().darker());//make dimmer
          g.fillRect(i*DIM*2,DIM*10,DIM*2,DIM*2);
          g=setTextColor(g);
-         g.drawString("HP:"+players[i].getHP(),i*DIM*2+5,DIM*11);
-         g.drawString("Bag:"+players[i].getBag(),i*DIM*2+5,DIM*11+11);
+         g.drawString("HP:"+players[i].getHP(),i*DIM*2+5,DIM*10+15);
+         g.drawString("Bag:",i*DIM*2+5,DIM*10+30);
+         for(int j=0;j<players[i].getBag().size();j++)
+         {
+            g.drawString(players[i].getBag().get(j),i*DIM*2,DIM*10+30*(j+2));
+         }
       }
    }
+   //pre: x is Graphics with color to be inverted
+   //post: x has a rgb-inverted color
    private Graphics setTextColor(Graphics x)
    {
       int r=x.getColor().getRed();
@@ -508,6 +566,8 @@ public class QuestBoard extends JPanel implements MouseListener
          }
       }
    }
+   
+   //these do absolutely nothing.
    public void mouseDragged( MouseEvent e){}
    public void mouseExited( MouseEvent e ){}
    public void mousePressed( MouseEvent e ){}
--- QuestDriver.java
+++ QuestDriver.java
@@ -4,9 +4,10 @@ public class QuestDriver
    private static QuestBoard board;
    public static void main(String[] args)
    {
-      board = new QuestBoard(1);
+      Object[] options = {1,2,3,4};
+      board = new QuestBoard(2);
       JFrame frame = new JFrame("Dragonfire Journey");
-      frame.setSize(board.getDIM()*13, (int)(board.getDIM()*12.7));
+      frame.setSize((int)(board.getDIM()*13.3), (int)(board.getDIM()*13));
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setContentPane(board);
       frame.setVisible(true);
