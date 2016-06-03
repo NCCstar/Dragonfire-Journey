d784ff10d499a256e9bf81ed0f9f4105a27d0b46
NCCstar <rhyspmiller@gmail.com>
2016-06-03 13:35:37 -0400

    Unbreakable driver

--- QuestDriver.java
+++ QuestDriver.java
@@ -5,7 +5,15 @@ public class QuestDriver
    public static void main(String[] args)
    {
       Object[] options = {1,2,3,4};
-      board = new QuestBoard((Integer)(JOptionPane.showInputDialog(null,"How many players?","Players",JOptionPane.INFORMATION_MESSAGE, null,options, options[0])));
+      while(true)
+      {
+         try
+         {
+            board = new QuestBoard((Integer)(JOptionPane.showInputDialog(null,"How many players?","Players",JOptionPane.INFORMATION_MESSAGE, null,options, options[0])));
+            break;
+         }
+         catch(Exception e){}
+      }
       JFrame frame = new JFrame("Dragonfire Journey");
       frame.setSize((int)(board.getDIM()*13.3), (int)(board.getDIM()*13));
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
